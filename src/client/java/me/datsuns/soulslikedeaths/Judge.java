a40a2c5f39e44f1604a3643cccc7ef022b69695c
datsuns <the.skeleton7@gmail.com>
2024-08-26 23:41:03 +0900

    refactor: change thres hold for head shot into constant variable

--- src/client/java/me/datsuns/soulslikedeaths/Judge.java
+++ src/client/java/me/datsuns/soulslikedeaths/Judge.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class Judge {
+    public final double HeightThreshold = 1.5;
     public boolean onTick(PlayerEntity p){
         if( p.isWet() ) {
             return SoulslikeDeathsClient.cfg.deathInWater;
@@ -16,7 +17,7 @@ public class Judge {
             return true;
         }
         if( SoulslikeDeathsClient.cfg.deathByHeadShot ) {
-            double threshold = player.getY() + 1.5;
+            double threshold = player.getY() + HeightThreshold;
             if( source.getPosition().getY() > threshold ) {
                 return true;
             }
